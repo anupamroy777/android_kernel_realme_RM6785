b57c2a0365924aa37b91caa950e4ceaa2297911f
Tobin C. Harding <me@tobin.cc>
2018-06-22 09:15:31 +1000

    UPSTREAM: random: Fix whitespace pre random-bytes work
    commit 8ddd6efa56c3fe23df9fe4cf5e2b49cc55416ef4 upstream.
    
    There are a couple of whitespace issues around the function
    get_random_bytes_arch().  In preparation for patching this function
    let's clean them up.
    
    Acked-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1745,7 +1745,7 @@ void get_random_bytes_arch(void *buf, int nbytes)
 
 		if (!arch_get_random_long(&v))
 			break;
-		
+
 		memcpy(p, &v, chunk);
 		p += chunk;
 		nbytes -= chunk;
@@ -1755,7 +1755,6 @@ void get_random_bytes_arch(void *buf, int nbytes)
 		get_random_bytes(p, nbytes);
 }
 EXPORT_SYMBOL(get_random_bytes_arch);
-
 
 /*
  * init_std_data - initialize pool with system data
