5d19369cc2f9e906b3839ead654475438c6e1f42
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-12 02:08:55 +0530

    block: zram_drv: always reset zram bdev
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1997,6 +1997,10 @@ static int zram_add(void)
 		zram_devices = zram;
 
 	pr_info("Added device: %s\n", zram->disk->disk_name);
+
+	pr_info("Resetting zram bdev");
+	reset_bdev(zram);
+
 	return device_id;
 
 out_free_queue:
