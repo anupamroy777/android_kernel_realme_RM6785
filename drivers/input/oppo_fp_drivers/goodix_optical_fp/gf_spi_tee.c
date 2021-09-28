07eddf7f8565b50e9c0b6ed8b4784e0e92d400c4
Sultanxda <sultanxda@gmail.com>
2021-09-28 10:21:55 +0530

    drivers: fingerprint: goodix: Set fingerprintd priority to max when screen is off
    Give fingerprintd the highest priority (MIN_NICE) when the screen is off to
    speed up fingerprint processing, and then reset its priority back to normal
    when the screen is on.
    
    There are now 2 fingerprint related processes:
     - android.hardware.biometrics.fingerprint@2.1-service -> comm:fingerprint@2.1
     - com.qualcomm.qti.biometrics.fingerprint.service -> comm:erprint.service
    
    For the match to work properly, use strstr() with "erprint".
    
    Change-Id: Ice4a384f99ae4201b2e5e942b0c8cc16c1190f52
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- drivers/input/oppo_fp_drivers/goodix_optical_fp/gf_spi_tee.c
+++ drivers/input/oppo_fp_drivers/goodix_optical_fp/gf_spi_tee.c
@@ -742,6 +742,19 @@ static const struct file_operations gf_fops = {
 #ifndef MTK_ONSCREENFINGERPRINT_EVENT
 #define MTK_ONSCREENFINGERPRINT_EVENT 20
 #endif
+
+static void set_fingerprintd_nice(int nice)
+{
+	struct task_struct *p;
+
+	read_lock(&tasklist_lock);
+	for_each_process(p) {
+		if (strstr(p->comm, "erprint"))
+			set_user_nice(p, nice);
+	}
+	read_unlock(&tasklist_lock);
+}
+
 static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		unsigned long val, void *data)
 {
@@ -782,6 +795,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		blank = *(int *)(evdata->data);
 		switch (blank) {
 		case FB_BLANK_POWERDOWN:
+			set_fingerprintd_nice(MIN_NICE);
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 1;
 #if defined(GF_NETLINK_ENABLE)
@@ -795,6 +809,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 			}
 			break;
 		case FB_BLANK_UNBLANK:
+			set_fingerprintd_nice(0);
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 0;
 #if defined(GF_NETLINK_ENABLE)
