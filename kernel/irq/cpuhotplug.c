9b513feff23daf15d4de00365f5e248000997b39
Jebaitedneko <Jebaitedneko@gmail.com>
2021-10-07 14:33:35 +0800

    kernel/irq: silence irq no longer affine to cpu logspam
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -134,7 +134,7 @@ static bool migrate_one_irq(struct irq_desc *desc)
 	 */
 	err = irq_do_set_affinity(d, affinity, false);
 	if (err) {
-		pr_warn_ratelimited("IRQ%u: set affinity failed(%d).\n",
+		pr_debug_ratelimited("IRQ%u: set affinity failed(%d).\n",
 				    d->irq, err);
 		brokeaff = false;
 	}
@@ -170,7 +170,7 @@ void irq_migrate_all_off_this_cpu(void)
 #ifndef OPLUS_FEATURE_CHG_BASIC
 // Nanwei.Deng@BSP.CHG.Basic, 2018/07/13  Add for delete log in release version
 		if (affinity_broken) {
-			pr_warn_ratelimited("IRQ %u: no longer affine to CPU%u\n",
+			pr_debug_ratelimited("IRQ %u: no longer affine to CPU%u\n",
 					    irq, smp_processor_id());
 		}
 #else
