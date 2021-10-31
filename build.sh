dc1e8de2defdcd7db0a8712e22f32b3b955a07d4
Johny8988 <github.769@gmail.com>
2021-10-31 05:04:36 +0000

    Update: build_script: Let AnyKernel Directory To Be Removed While Running Script

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_ho
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
-[ -d "out" ] && rm -rf out || mkdir -p out
+[ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 RMX2151_defconfig
 
