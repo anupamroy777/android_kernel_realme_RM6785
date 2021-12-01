JamiKettunen/android_kernel_volla_mt6763

tools: dct: Move to modern Python 3
Now we can drop the Python 2 build-time dependency of this kernel tree!
\o/

tools/dct/DrvGen.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python2
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -42,7 +42,7 @@ from utility.util import LogLevel
 from utility.util import log
 
 def usage():
-    print '''
+    print('''
 usage: DrvGen [dws_path] [file_path] [log_path] [paras]...
 
 options and arguments:
@@ -51,7 +51,7 @@ dws_path    :    dws file path
 file_path   :    where you want to put generated files
 log_path    :    where to store the log files
 paras        :    parameter for generate wanted file
-'''
+''')
 
 def is_oldDws(path, gen_spec):
     if not os.path.exists(path):
@@ -60,7 +60,7 @@ def is_oldDws(path, gen_spec):
 
     try:
         root = xml.dom.minidom.parse(dws_path)
-    except Exception, e:
+    except Exception:
         log(LogLevel.warn, '%s is not xml format, try to use old DCT!' %(dws_path))
         if len(gen_spec) == 0:
             log(LogLevel.warn, 'Please use old DCT UI to gen all files!')
@@ -139,32 +139,32 @@ if __name__ == '__main__':
     chipId = ChipObj.get_chipId(dws_path)
     log(LogLevel.info, 'chip id: %s' %(chipId))
     chipObj = None
-    if cmp(chipId, 'MT6797') == 0:
+    if chipId == 'MT6797':
         chipObj = Everest(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757') == 0:
+    elif chipId == 'MT6757':
         chipObj = Olympus(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757-P25') == 0:
+    elif chipId == 'MT6757-P25':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'KIBOPLUS') == 0:
+    elif chipId == 'KIBOPLUS':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'MT6570') == 0:
+    elif chipId == 'MT6570':
         chipObj = Rushmore(dws_path, gen_path)
-    elif cmp(chipId, 'MT6799') == 0:
+    elif chipId == 'MT6799':
         chipObj = Whitney(dws_path, gen_path)
-    elif cmp(chipId, 'MT6763') == 0:
+    elif chipId == 'MT6763':
         chipObj = MT6763(dws_path, gen_path)
-    elif cmp(chipId, 'MT6759') == 0:
+    elif chipId == 'MT6759':
         chipObj = MT6759(dws_path, gen_path)
-    elif cmp(chipId, 'MT6750S') == 0:
+    elif chipId == 'MT6750S':
         chipObj = MT6750S(dws_path, gen_path)
-    elif cmp(chipId, 'MT6758') == 0:
+    elif chipId == 'MT6758':
         chipObj = MT6758(dws_path, gen_path)
-    elif cmp(chipId, 'MT6739') == 0:
+    elif chipId == 'MT6739':
         chipObj = MT6739(dws_path, gen_path)
-    elif cmp(chipId, 'MT8695') == 0:
+    elif chipId == 'MT8695':
         chipObj = MT8695(dws_path, gen_path)
-    elif cmp(chipId, 'MT6771') == 0 or \
-         cmp(chipId, 'MT6775') == 0:
+    elif chipId == 'MT6771' or \
+         chipId == 'MT6775':
         chipObj = MT6771(dws_path, gen_path)
     #elif cmp(chipId, 'MT6775') == 0:
         #chipObj = MT6775(dws_path, gen_path)

tools/dct/obj/ChipObj.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -16,30 +16,30 @@ import os, sys
 import collections
 import xml.dom.minidom
 
-from GpioObj import GpioObj
-from GpioObj import GpioObj_whitney
-from GpioObj import GpioObj_MT6759
-from GpioObj import GpioObj_MT6739
-from GpioObj import GpioObj_MT6771
-from GpioObj import GpioObj_MT6763
-from EintObj import EintObj
-from EintObj import EintObj_MT6750S
-from EintObj import EintObj_MT6739
-from AdcObj import AdcObj
-from ClkObj import ClkObj
-from ClkObj import ClkObj_Everest
-from ClkObj import ClkObj_Olympus
-from ClkObj import ClkObj_Rushmore
-from I2cObj import I2cObj
-from I2cObj import I2cObj_MT6759
-from I2cObj import I2cObj_MT6775
-from PmicObj import PmicObj
-from PmicObj import PmicObj_MT6758
-from Md1EintObj import Md1EintObj
-from Md1EintObj import Md1EintObj_MT6739
-from PowerObj import PowerObj
-from KpdObj import KpdObj
-from ModuleObj import ModuleObj
+from obj.GpioObj import GpioObj
+from obj.GpioObj import GpioObj_whitney
+from obj.GpioObj import GpioObj_MT6759
+from obj.GpioObj import GpioObj_MT6739
+from obj.GpioObj import GpioObj_MT6771
+from obj.GpioObj import GpioObj_MT6763
+from obj.EintObj import EintObj
+from obj.EintObj import EintObj_MT6750S
+from obj.EintObj import EintObj_MT6739
+from obj.AdcObj import AdcObj
+from obj.ClkObj import ClkObj
+from obj.ClkObj import ClkObj_Everest
+from obj.ClkObj import ClkObj_Olympus
+from obj.ClkObj import ClkObj_Rushmore
+from obj.I2cObj import I2cObj
+from obj.I2cObj import I2cObj_MT6759
+from obj.I2cObj import I2cObj_MT6775
+from obj.PmicObj import PmicObj
+from obj.PmicObj import PmicObj_MT6758
+from obj.Md1EintObj import Md1EintObj
+from obj.Md1EintObj import Md1EintObj_MT6739
+from obj.PowerObj import PowerObj
+from obj.KpdObj import KpdObj
+from obj.ModuleObj import ModuleObj
 
 from utility.util import log
 from utility.util import LogLevel
@@ -166,7 +166,7 @@ class ChipObj:
             # return True
 
         for para in paras:
-            if cmp(para, 'cust_dtsi') == 0:
+            if para == 'cust_dtsi':
                 self.gen_custDtsi()
                 continue
 
@@ -198,7 +198,7 @@ class ChipObj:
         #sorted_list = sorted(self.__objs.keys())
         #for tag in sorted_list:
         for tag in self.__objs.keys():
-            if cmp(tag, 'gpio') == 0:
+            if tag == 'gpio':
                 gpioObj = self.create_obj(tag)
                 gen_str += ModuleObj.writeHeader(gpioObj.get_dtsiFileName())
                 gen_str += gpioObj.fill_mapping_dtsiFile()

tools/dct/utility/util.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -28,16 +28,16 @@ class LogLevel:
 def log(level, msg):
     if level == LogLevel.info:
         if DEBUG:
-            print LEVEL_INFO + msg
+            print(LEVEL_INFO + msg)
     elif level == LogLevel.warn:
-        print LEVEL_WARN + msg
+        print(LEVEL_WARN + msg)
     elif level == LogLevel.error:
-        print LEVEL_ERROR + msg
+        print(LEVEL_ERROR + msg)
 
 def compare(value):
     lst = re.findall(r'\d+', value)
     if len(lst) != 0:
-        return string.atoi(lst[0])
+        return int(lst[0])
 
     # if can not find numbers
     return value

anupamroy777/android_kernel_realme_RM6785

tools/dct/DrvGen.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -47,7 +47,7 @@ from utility.util import LogLevel
 from utility.util import log
 
 def usage():
-    print '''
+    print('''
 usage: DrvGen [dws_path] [file_path] [log_path] [paras]...
 
 options and arguments:
@@ -56,7 +56,7 @@ dws_path    :    dws file path
 file_path   :    where you want to put generated files
 log_path    :    where to store the log files
 paras        :    parameter for generate wanted file
-'''
+''')
 
 def is_oldDws(path, gen_spec):
     if not os.path.exists(path):
@@ -65,7 +65,7 @@ def is_oldDws(path, gen_spec):
 
     try:
         root = xml.dom.minidom.parse(dws_path)
-    except Exception, e:
+    except Exception:
         log(LogLevel.warn, '%s is not xml format, try to use old DCT!' %(dws_path))
         if len(gen_spec) == 0:
             log(LogLevel.warn, 'Please use old DCT UI to gen all files!')
@@ -144,46 +144,46 @@ if __name__ == '__main__':
     chipId = ChipObj.get_chipId(dws_path)
     log(LogLevel.info, 'chip id: %s' %(chipId))
     chipObj = None
-    if cmp(chipId, 'MT6797') == 0:
+    if chipId == 'MT6797':
         chipObj = MT6797(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757') == 0:
+    elif chipId == 'MT6757':
         chipObj = MT6757(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757-P25') == 0:
+    elif chipId == 'MT6757-P25':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'MT6570') == 0:
+    elif chipId == 'MT6570':
         chipObj = MT6570(dws_path, gen_path)
-    elif cmp(chipId, 'MT6799') == 0:
+    elif chipId == 'MT6799':
         chipObj = MT6799(dws_path, gen_path)
-    elif cmp(chipId, 'MT6763') == 0:
+    elif chipId == 'MT6763':
         chipObj = MT6763(dws_path, gen_path)
-    elif cmp(chipId, 'MT6759') == 0:
+    elif chipId == 'MT6759':
         chipObj = MT6759(dws_path, gen_path)
-    elif cmp(chipId, 'MT6750S') == 0:
+    elif chipId == 'MT6750S':
         chipObj = MT6750S(dws_path, gen_path)
-    elif cmp(chipId, 'MT6758') == 0:
+    elif chipId == 'MT6758':
         chipObj = MT6758(dws_path, gen_path)
-    elif cmp(chipId, 'MT6739') == 0:
+    elif chipId == 'MT6739':
         chipObj = MT6739(dws_path, gen_path)
-    elif cmp(chipId, 'MT8695') == 0 or \
-         cmp(chipId, 'MT8168') == 0:
+    elif chipId == 'MT8695' or \
+         chipId == 'MT8168':
         chipObj = MT8695(dws_path, gen_path)
-    elif cmp(chipId, 'MT6771') == 0 or \
-         cmp(chipId, 'MT6775') == 0 or \
-         cmp(chipId, 'MT6765') == 0 or \
-         cmp(chipId, 'MT3967') == 0 or \
-         cmp(chipId, 'MT6761') == 0:
+    elif chipId == 'MT6771' or \
+         chipId == 'MT6775' or \
+         chipId == 'MT6765' or \
+         chipId == 'MT3967' or \
+         chipId == 'MT6761':
         chipObj = MT6771(dws_path, gen_path)
-    elif cmp(chipId, 'MT6779') == 0:
+    elif chipId == 'MT6779':
         chipObj = MT6779(dws_path, gen_path)
-    elif cmp(chipId, 'MT6768') == 0:
+    elif chipId == 'MT6768':
         chipObj = MT6768(dws_path, gen_path)
-    elif cmp(chipId, 'MT6785') == 0:
+    elif chipId == 'MT6785':
         chipObj = MT6785(dws_path, gen_path)
-    elif cmp(chipId, 'MT6885') == 0 or \
-         cmp(chipId, 'MT6873') == 0 or \
-         cmp(chipId, 'MT6893') == 0:
+    elif chipId == 'MT6885' or \
+         chipId == 'MT6873' or \
+         chipId == 'MT6893':
         chipObj = MT6885(dws_path, gen_path)
-    elif cmp(chipId, 'MT6853') == 0:
+    elif chipId == 'MT6853':
         chipObj = MT6853(dws_path, gen_path)
     else:
         chipObj = ChipObj(dws_path, gen_path)

tools/dct/obj/ChipObj.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -16,43 +16,43 @@ import os, sys
 import collections
 import xml.dom.minidom
 
-from GpioObj import GpioObj
-from GpioObj import GpioObj_MT6799
-from GpioObj import GpioObj_MT6759
-from GpioObj import GpioObj_MT6739
-from GpioObj import GpioObj_MT6771
-from GpioObj import GpioObj_MT6763
-from GpioObj import GpioObj_MT6768
-from GpioObj import GpioObj_MT6785
+from obj.GpioObj import GpioObj
+from obj.GpioObj import GpioObj_MT6799
+from obj.GpioObj import GpioObj_MT6759
+from obj.GpioObj import GpioObj_MT6739
+from obj.GpioObj import GpioObj_MT6771
+from obj.GpioObj import GpioObj_MT6763
+from obj.GpioObj import GpioObj_MT6768
+from obj.GpioObj import GpioObj_MT6785
 
-from EintObj import EintObj
-from EintObj import EintObj_MT6750S
-from EintObj import EintObj_MT6739
-from EintObj import EintObj_MT6885
-from EintObj import EintObj_MT6853
+from obj.EintObj import EintObj
+from obj.EintObj import EintObj_MT6750S
+from obj.EintObj import EintObj_MT6739
+from obj.EintObj import EintObj_MT6885
+from obj.EintObj import EintObj_MT6853
 
-from AdcObj import AdcObj
-from AdcObj import AdcObj_MT6785
+from obj.AdcObj import AdcObj
+from obj.AdcObj import AdcObj_MT6785
 
-from ClkObj import ClkObj
-from ClkObj import ClkObj_MT6797
-from ClkObj import ClkObj_MT6757
-from ClkObj import ClkObj_MT6570
-from ClkObj import ClkObj_MT6779
+from obj.ClkObj import ClkObj
+from obj.ClkObj import ClkObj_MT6797
+from obj.ClkObj import ClkObj_MT6757
+from obj.ClkObj import ClkObj_MT6570
+from obj.ClkObj import ClkObj_MT6779
 
-from I2cObj import I2cObj
-from I2cObj import I2cObj_MT6759
-from I2cObj import I2cObj_MT6775
+from obj.I2cObj import I2cObj
+from obj.I2cObj import I2cObj_MT6759
+from obj.I2cObj import I2cObj_MT6775
 
-from PmicObj import PmicObj
-from PmicObj import PmicObj_MT6758
+from obj.PmicObj import PmicObj
+from obj.PmicObj import PmicObj_MT6758
 
-from Md1EintObj import Md1EintObj
-from Md1EintObj import Md1EintObj_MT6739
-from PowerObj import PowerObj
-from KpdObj import KpdObj
-from RfioObj import RfioObj
-from ModuleObj import ModuleObj
+from obj.Md1EintObj import Md1EintObj
+from obj.Md1EintObj import Md1EintObj_MT6739
+from obj.PowerObj import PowerObj
+from obj.KpdObj import KpdObj
+from obj.RfioObj import RfioObj
+from obj.ModuleObj import ModuleObj
 
 from utility.util import log
 from utility.util import LogLevel
@@ -151,7 +151,7 @@ class ChipObj:
 
         # initialize the objects mapping table
         self.init_objs()
-        # get module nodes from DWS file
+        # get module nodes from .DWS file
         nodes = node[0].getElementsByTagName('module')
 
         for node in nodes:
@@ -190,7 +190,7 @@ class ChipObj:
             # return True
 
         for para in paras:
-            if cmp(para, 'cust_dtsi') == 0:
+            if para == 'cust_dtsi':
                 self.gen_custDtsi()
                 continue
 
@@ -228,7 +228,7 @@ class ChipObj:
         #sorted_list = sorted(self.__objs.keys())
         #for tag in sorted_list:
         for tag in self.__objs.keys():
-            if cmp(tag, 'gpio') == 0:
+            if tag == 'gpio':
                 gpioObj = self.create_obj(tag)
                 gen_str += ModuleObj.writeHeader(gpioObj.get_dtsiFileName())
                 gen_str += gpioObj.fill_mapping_dtsiFile()

tools/dct/utility/util.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -27,16 +27,16 @@ class LogLevel:
 
 def log(level, msg):
     if level == LogLevel.info:
-        print LEVEL_INFO + msg
+        print(LEVEL_INFO + msg)
     elif level == LogLevel.warn:
-        print LEVEL_WARN + msg
+        print (LEVEL_WARN + msg)
     elif level == LogLevel.error:
-        print LEVEL_ERROR + msg
+        print (LEVEL_ERROR + msg)
 
 def compare(value):
     lst = re.findall(r'\d+', value)
     if len(lst) != 0:
-        return string.atoi(lst[0])
+        return int(lst[0])
 
     # if can not find numbers
     return value