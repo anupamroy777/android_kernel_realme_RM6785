1060967cb6812a3c832266287e43bdae27ff680e
Johny8988 <github.769@gmail.com>
2021-12-01 09:28:33 +0000

    Enabled Clang Lto In Build Script And Defconfig

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ ccache -M 100G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=ThunderStorm
 export KBUILD_BUILD_USER="Gorilla669"
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
+git clone --depth=1 https://github.com/Kdrag0n/proton-clang.git clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
@@ -17,13 +17,14 @@ git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86
 
 make O=out ARCH=arm64 RMX2151_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
+                      LD=ld.lld \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
@@ -32,9 +33,9 @@ function zupload()
 git clone --depth=1 https://github.com/Johny8988/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 ThunderStorm-KERNEL-RMX2151.zip *
+zip -r9 ThunderStorm-lto-KERNEL-RMX2151.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer wet ThunderStorm-KERNEL-RMX2151.zip
+./transfer wet ThunderStorm-lto-KERNEL-RMX2151.zip
 }
 
 compile
