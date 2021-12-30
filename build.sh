01c9549ddc88977c15fb995b67a72c104201a567
Johny8988 <github.769@gmail.com>
2021-12-30 07:52:11 +0000

    build_script: remove unneeded compilers

--- build.sh
+++ build.sh
@@ -10,8 +10,6 @@ export ARCH=arm64
 export KBUILD_BUILD_HOST=Anupam_Roy
 export KBUILD_BUILD_USER="Gorilla669"
 git clone --depth=1 https://github.com/Kdrag0n/proton-clang.git clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 [ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
 
