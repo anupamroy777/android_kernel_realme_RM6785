788a2e3776fe805fe3a5a7040ab2aeb6cd533481
anupamroy777 <github.769@gmail.com>
2022-05-08 21:47:53 +0000

    build_script: add option to take error log
    Signed-off-by: anupamroy777 <github.769@gmail.com>

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ make -j$(nproc --all) O=out \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
                       CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 }
 
 function zupload()
@@ -36,9 +36,9 @@ git clone --depth=1 https://github.com/Johny8988/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 date=$(date "+%Y-%m-%d")
-zip -r9 ThunderStorm-lto-KERNEL-RM6785.zip *
+zip -r9 ThunderStorm-lto-KERNEL-RM6785-$date.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer wet ThunderStorm-lto-KERNEL-RM6785.zip
+./transfer wet ThunderStorm-lto-KERNEL-RM6785-$date.zip
 }
 
 compile
