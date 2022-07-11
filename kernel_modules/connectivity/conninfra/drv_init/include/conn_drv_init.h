33d6d474502082bc0723482911e09c7937335c74
Anupam Roy <github.769@gmail.com>
2022-07-11 02:55:11 +0000

    Revert "Import kernel_modules from Realme7_A11 source"
    This reverts commit e520c37d501ed1152d84d09d16668a1a7f922407.
    
    Signed-off-by: Anupam Roy <github.769@gmail.com>

--- kernel_modules/connectivity/conninfra/drv_init/include/conn_drv_init.h
+++
@@ -1,9 +0,0 @@
-/*  SPDX-License-Identifier: GPL-2.0 */
-/*
- * Copyright (c) 2019 MediaTek Inc.
- */
-
-#ifndef _CONNECTIVITY_DRV_INIT_H_
-#define _CONNECTIVITY_DRV_INIT_H_
-extern int do_connectivity_driver_init(int chip_id);
-#endif
