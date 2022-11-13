Neternels/android_kernel_realme_rmx2020

fs/exec: Affine fingerprint service to big cpus
Affining the fingerprint service to big CPUs would reduce latency.

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

fs/exec.c
@@ -82,6 +82,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
+#define FP_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.RMX2020"
 static struct signal_struct *zygote32_sig;
 static struct signal_struct *zygote64_sig;
 
@@ -1852,6 +1853,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
+		else if (unlikely(!strncmp(filename->name,
+					  FP_BIN_PREFIX,
+					  strlen(FP_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}

anupamroy777/android_kernel_realme_RM6785

Signed-off-by: anupamroy777 <github.769@gmail.com>

fs/exec.c
@@ -83,6 +83,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
+#define FP_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.RM6785"
 static struct signal_struct *zygote32_sig;
 static struct signal_struct *zygote64_sig;
 
@@ -1872,6 +1873,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
+		else if (unlikely(!strncmp(filename->name,
+					  FP_BIN_PREFIX,
+					  strlen(FP_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}