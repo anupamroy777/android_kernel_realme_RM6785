01ba456c05137ba791a0fb8c0d37cdb3a72c00b2
anupamroy777 <github.769@gmail.com>
2023-09-11 20:13:32 +0000

    fs: add KernelSU syscalls
    Signed-off-by: anupamroy777 <github.769@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1729,11 +1729,18 @@ extern int oplus_exec_block(struct file *file);
 /*
  * sys_execve() executes a new program.
  */
+
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,void *envp, int *flags);
+#endif
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#ifdef CONFIG_KSU
+  ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
