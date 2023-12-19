e1ef2b81f01444ed8f1658bbe3a23f02f30332e1
anupamroy777 <github.769@gmail.com>
2023-12-19 18:17:10 +0530

    drivers/base/power: Suppress POWER DEBUG logging
    Signed-off-by: anupamroy777 <github.769@gmail.com>

--- drivers/base/power/oppo_pwr_dbg/debug_load.c
+++ drivers/base/power/oppo_pwr_dbg/debug_load.c
@@ -34,7 +34,7 @@
 #include "debug_load.h"
 
 #define POWER_TUNER_TAG "[POWER_DEBUG]"
-#define POWER_TUNER_INFO(fmt, args...) pr_info(POWER_TUNER_TAG fmt, ##args)
+#define POWER_TUNER_INFO(fmt, args...) pr_debug(POWER_TUNER_TAG fmt, ##args)
 #define POWER_TUNER_ERR(fmt, args...) pr_info(POWER_TUNER_TAG fmt, ##args)
 #define MAX_PID (32768)
 #define CTP_WINDOW_SZ (5)
